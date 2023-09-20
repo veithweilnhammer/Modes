548911b944e238a1b8a1f0aadd5966a9eccb6de0
unknown <veith.weilnhammer@gmail.com>
2023-09-19 21:17:15 -0700

    updated the figures and everything

--- Functions/f_compute_mouse_group_acf.R
+++ Functions/f_compute_mouse_group_acf.R
@@ -12,13 +12,14 @@ f_compute_mouse_group_acf <- function(PwData, acf_to_test, max_trial) {
       print(trial_idx)
       PwTest <- data.frame()
       Y = double()
-      PwTest <- PwData[PwData$Trial == trial_idx , colnames(PwData) %in% c(paste(acf_to_test[acf_idx]), 'diff_acf_External', 'session_id')]
+      PwTest <- PwData[PwData$Trial == trial_idx , colnames(PwData) %in% c(paste(acf_to_test[acf_idx]), 'diff_acf_External', 'diff_acf_Difficulty', 'session_id')]
       assign("Y", PwTest[,acf_to_test[acf_idx]])
       PwTest$Y <- exclude_3SD(Y)
 
-      MEM <- summary(lmer(Y ~ 1 + diff_acf_External + (1|session_id) , data = PwTest))
-
-      if (MEM$coefficients[9] < 0.05){
+      #MEM <- summary(lmer(Y ~ 1 + diff_acf_External + (1|session_id) , data = PwTest))
+      MEM <- summary(lmer(Y ~ 1 + diff_acf_External + diff_acf_Difficulty + (1|session_id), data = PwTest))
+      
+      if (MEM$coefficients[13] < 0.05){
         p_Y[trial_idx-1] ="< 0.05"
       } else {
         p_Y[trial_idx-1] = "> 0.05"
